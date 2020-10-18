f902bae476a090979fbb492b5382e8cea3d1df7a
Animesh Bhakat <bhakat.animesh1024@gmail.com>
2020-10-18 19:51:54 +0530

    added typing effect

--- js/main.js
+++ js/main.js
@@ -1,9 +1,3 @@
-/**
-* Template Name: Personal - v2.3.0
-* Template URL: https://bootstrapmade.com/personal-free-resume-bootstrap-template/
-* Author: BootstrapMade.com
-* License: https://bootstrapmade.com/license/
-*/
 !(function($) {
   "use strict";
 
@@ -53,6 +47,50 @@
       }
     }
   });
+  
+	const typedTextSpan = document.querySelector(".typed-text");
+	const cursorSpan = document.querySelector(".cursor");
+	
+	const textArray = ["Competitive Programmer", "Learner", "FOSS Enthusiast", "Web Enthusiast"]
+	const typingDelay = 100;
+	const erasingDelay = 100;
+	const newTextDelay = 1000;
+	let textArrayIndex = 0;
+	let charIndex = 0;
+	
+	function type(){
+		if(charIndex < textArray[textArrayIndex].length){
+			if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+			typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+			charIndex++;
+			setTimeout(type, typingDelay);
+		}
+		else{
+			cursorSpan.classList.remove("typing");
+			setTimeout(erase, newTextDelay);
+		}
+	}
+	
+	function erase(){
+		if(charIndex > 0){
+			if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+			typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex - 1);
+			charIndex--;
+			setTimeout(erase, erasingDelay);
+		}
+		else{
+			cursorSpan.classList.remove("typing");
+			textArrayIndex++;
+			if(textArrayIndex >= textArray.length) textArrayIndex =0;
+			setTimeout(type, typingDelay + 1100);
+		}
+	}
+	
+	document.addEventListener("DOMContentLoaded", function(){
+		if(textArray.length) setTimeout(type, newTextDelay + 250);
+	});
+		
+	
 
   // Activate/show sections on load with hash links
   if (window.location.hash) {
